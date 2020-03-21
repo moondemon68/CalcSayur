d10bf2f364059a973f755a4b5c5783af3458212b
arfaghif <52845090+arfaghif@users.noreply.github.com>
2020-03-21 13:16:42 +0700

    nambahin pesan error sqrtexpression

--- SqrtExpression.cpp
+++ SqrtExpression.cpp
@@ -6,5 +6,14 @@ SqrtExpression::SqrtExpression(Expression* x) : UnaryExpression(x) {
 }
 
 double SqrtExpression::solve() {
+    try {
+        if (x->solve() < 0) {
+            throw "Value Error";
+        }
+    } catch (QString error) {
+        QErrorMessage* E = new QErrorMessage();
+        E->showMessage(error);
+    }
+    
     return sqrt(x->solve());
 }
